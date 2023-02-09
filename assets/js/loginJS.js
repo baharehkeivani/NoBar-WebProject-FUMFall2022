3878c1d9a6f9dde7dd1a366cd8ed333add1095d9
zahra-ardane <zahraardane2001@gmail.com>
2023-02-10 00:36:35 +0330

    login and register connected to api

--- assets/js/loginJS.js
+++ assets/js/loginJS.js
@@ -1,5 +1,8 @@
+// console.clear();
+// axios.defaults.withCredentials = true;
 
-console.clear();
+
+const baseRoute = "http://127.0.0.1:4000/v1";
 
 const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
@@ -27,5 +30,85 @@ signupBtn.addEventListener('click', e => {
     }
   });
 });
+
+let tempAccountType;
+
+const form = document.getElementById('signupForm');
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  data = {};
+  for (const [key, value] of formData) {
+    data[key] = value;
+  }
+
+  // console.log(data);
+  axios.post(`${baseRoute}/register`, data).then((response) => {
+    console.log("res ",response);
+    let temp = document.getElementById('error1');
+    if(!response.data.success) {
+      temp.style.display = "block";
+      temp.textContent = response.data.message;
+    } else {
+      temp.style.display = "none";
+    }
+  })
+  .catch((error) => console.log(error));
+
+});
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const formData = new FormData(loginForm);
+  data = {};
+  for (const [key, value] of formData) {
+    data[key] = value;
+  }
+
+  console.log(data);
+  axios.post(`${baseRoute}/login`, data).then((response) => {
+    //based on responses success an error must be shown or the page must be redirected to dashbord
+    console.log("res ",response);
+    let temp = document.getElementById('error1');
+    if(!response.data.success) {
+      temp.style.display = "block";
+      temp.textContent = response.data.message;
+    } else {
+      temp.style.display = "none";
+    }
+  })
+  .catch((error) => console.log(error));
+
+});
+
+
+// function signUp (event) {
+//   event.preventDefault()
+//   var data = new FormData();
+
+//   // (B2) APPEND FIELDS
+//   var all = document.querySelectorAll("#signupForm input, #user_form select"); //, #user_form textarea
+//   for (let field of all) {
+//     // EXCLUDE SUBMIT + BUTTONS
+//     if (field.type != "submit" && field.type != "button") { 
+//       // CHECKBOX + RADIO - MUST BE CHECKED
+//       // if (field.type=="radio" || field.type=="checkbox") {
+//       //   if (field.checked) { data.append(field.name, field.value); }
+//       // }
+//       // OTHER FIELDS
+//       // else { 
+//         data.append(field.name, field.value);
+//       //  }
+//     }
+//   }
+//   // (B3) WHATEVER YOU WANT TO DO NEXT
+//   // DO CHECKS, SEND TO SERVER, ETC...
+
+//   // for (let [k, v] of data.entries()) { console.log(k, v); }
+//   // return false;
+// }
+
 //# sourceURL=pen.js
-    
